1559c24c9a2e7d0918ebc5afce6b02cef5516ff8
Trent <suzukitrent@gmail.com>
2021-01-31 13:13:27 -0800

    All robots controlled by actors

--- demo.py
+++ demo.py
@@ -10,13 +10,19 @@ game.reset()
 
 actor0 = Actor(0)
 action0 = [1, 0, 0, 0, 0, 0, 0, 0]
+actor1 = Actor(1)
+action1 = [1, 0, 0, 0, 0, 0, 0, 0]
+actor2 = Actor(2)
+action2 = [1, 0, 0, 0, 0, 0, 0, 0]
+actor3 = Actor(3)
+action3 = [1, 0, 0, 0, 0, 0, 0, 0]
 # only when render = True
 for _ in range(1000):
-    obs, reward, done, info = game.step(np.array([action0,
-       [0, 0, 0, 0, 0, 0, 0, 1],
-       [0, 0, 0, 0, 0, 0, 0, 1],
-       [0, 0, 0, 0, 0, 0, 0, 1]]))
+    obs, reward, done, info = game.step(np.array([action0,action1,action2,action3]))
     action0 = actor0.action_from_state(obs, game.g_map)
+    action1 = actor1.action_from_state(obs, game.g_map)
+    action2 = actor2.action_from_state(obs, game.g_map)
+    action3 = actor3.action_from_state(obs, game.g_map)
 # game.play()
 
 #%%
