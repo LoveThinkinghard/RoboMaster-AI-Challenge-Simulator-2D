ed137f08c4bc3eec872d5fc93200e21d6275e83f
Trent <suzukitrent@gmail.com>
2021-01-31 12:55:11 -0800

    Created Actor class

--- demo.py
+++ demo.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
 from rmaics import rmaics
 from kernal import record_player
+from actor import Actor
+import numpy as np
 
 #%%
 game = rmaics(agent_num=4, render=True)
 game.reset()
+
+actor0 = Actor(0)
+action0 = [1, 0, 0, 0, 0, 0, 0, 0]
 # only when render = True
-game.play()
+for _ in range(1000):
+    obs, reward, done, info = game.step(np.array([action0,
+       [0, 0, 0, 0, 0, 0, 0, 1],
+       [0, 0, 0, 0, 0, 0, 0, 1],
+       [0, 0, 0, 0, 0, 0, 0, 1]]))
+    action0 = actor0.action_from_state(obs, game.g_map)
+# game.play()
 
 #%%
 game.save_record('./records/record0.npy')
